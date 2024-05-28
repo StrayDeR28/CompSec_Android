1d6299e7d32597355cf750923aacf05023458666
nickname838 <german.galitskov@gmail.com>
2024-05-28 21:59:22 +0700

    Design fixes

--- Assets/Scipts/MenuScore.cs
+++ Assets/Scipts/MenuScore.cs
@@ -5,12 +5,16 @@ public class MenuScore : MonoBehaviour
 {
     [SerializeField] TMP_Text score;
 
+    // private void Awake() //для очистки
+    // {
+    //     PlayerPrefs.DeleteKey("TotalPointsForTest1");
+    // }
     void Start()
     {
         if (PlayerPrefs.HasKey("TotalPointsForTest1"))
         {
-            gameObject.SetActive(true);
-            score.text = PlayerPrefs.GetInt("TotalPointsForTest1").ToString();
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            score.text = PlayerPrefs.GetInt("TotalPointsForTest1").ToString() + "/40";
             print("TotalPointsForTest1 exists, value = " + PlayerPrefs.GetInt("TotalPointsForTest1"));
         }
     }
